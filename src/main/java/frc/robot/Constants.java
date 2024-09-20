736b5b3349bc34efff792fa1ce3552664ea09811
ToddFerris <97573112+ToddFerris@users.noreply.github.com>
2024-09-19 19:58:16 -0700

    Turn speed needs tuning

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -38,10 +38,10 @@ public final class Constants {
                 * WHEEL_CIRCUMFERENCE;
 
         // Speeds for the robot when moving, all are in Meters/Second
-        public static final double TELEOP_MAX_SPEED_METERS_PER_SECOND = 5; // Max is 5.5435
-        public static final double AUTO_MAX_SPEED_METERS_PER_SECOND = 3;
-        public static final double teleOpNormalAngularSpeed = 3 * (2 * Math.PI);
-        public static final double teleOpSlowAngularSpeed = 1 * (2 * Math.PI);
+        public static final double TELEOP_MAX_SPEED_METERS_PER_SECOND = 0.5; // Max is 5.5435
+        public static final double AUTO_MAX_SPEED_METERS_PER_SECOND = 0.3;
+        public static final double teleOpNormalAngularSpeed = 0.3;
+        public static final double teleOpSlowAngularSpeed = 0.1;
 
         public static final double DRIVE_ENCODER_TO_METERS = (WHEEL_CIRCUMFERENCE / (DRIVE_GEAR_RATIO * 2048.0));
         public static final double DRIVE_ENCODER_VELOCITY_TO_METERS_PER_SECOND = (600.0 * WHEEL_CIRCUMFERENCE)
